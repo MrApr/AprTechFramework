f1d121f3606d17ebbe3b56a48d440a702cc293e5
mr_apr_ <mamali0938@gmail.com>
2020-07-22 18:26:17 +0430

    base structor files

---
+++ bootstrap/BaseFiles/Classes/PDOHelper.php
@@ -0,0 +1,35 @@
+<?php
+
+
+class PDOHelper
+{
+    /**
+     * Name of host that db works on
+     * @var
+     */
+    private $host;
+
+    /**
+     * Related user that connects to db
+     * @var
+     */
+    private $user;
+
+    /**
+     * user pass to get connect to that db
+     * @var
+     */
+    private $pass;
+
+    /**
+     * name of related database
+     * @var
+     */
+    private $database;
+
+
+    public function __construct()
+    {
+
+    }
+}
---
+++ config/database.php
@@ -0,0 +1,5 @@
+<?php
+define("DB_HOST","localhost");
+define("DB_USER","heroes");
+define("DB_PASS","22510070");
+define("DATABASE","dating_portal");
