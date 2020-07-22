698e3c727407247e5aad925fd552de48b05595f2
mr_apr_ <mamali0938@gmail.com>
2020-07-22 19:32:07 +0430

    Finished PDO Helper file in order to use pdo class more easily with less code

--- bootstrap/BaseFiles/Classes/PDOHelper.php
+++ bootstrap/BaseFiles/Classes/PDOHelper.php
@@ -27,9 +27,105 @@ class PDOHelper
      */
     private $database;
 
+    /**
+     * stores and keeps pdo connection in order to execute queries
+     * @var
+     */
+    private $pdo_connection;
 
+    /**
+     * Contains pdo statement in order to get executed
+     * @var
+     */
+    private $pdo_statement;
+
+    /**
+     * An array that contains values in for pdo execution statement
+     * @var
+     */
+    private $pdo_stmt_values = [];
+
+
+    /**
+     * Set default values in
+     * PDOHelper constructor.
+     */
     public function __construct()
     {
-
+        $this->database = "new_bookstore";
+        $this->host = "localhost";
+        $this->user = "heroes";
+        $this->pass = "22510070";
+/*        $this->database = DATABASE;
+        $this->host = DB_HOST;
+        $this->user = DB_USER;
+        $this->pass = DB_PASS;*/
     }
+
+    /**
+     * Creates a new pdo connection and stores it in $pdo property;
+     */
+    public function connectToDB()
+    {
+        $dsn = "mysql:host={$this->host};dbname={$this->database}";
+        try{
+            $pdo = new PDO($dsn,$this->user,$this->pass);
+            $this->pdo_connection = $pdo;
+            unset($pdo);
+        }catch (PDOException $e)
+        {
+            die("cannot connect to pdo with error: ".$e->getMessage());
+        }
+    }
+
+    /**
+     * Config pdo and set desired options for PDO
+     */
+    public function setOptionsToPDOConnection()
+    {
+        $this->pdo_connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
+        $this->pdo_connection->setAttribute(PDO::ATTR_PERSISTENT,true);
+        $this->pdo_connection->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    }
+
+    /**
+     * Prepare PDO query statement to get executed
+     * @param string $query
+     * @param array|null $values
+     */
+    public function prepareQuery(string $query, array $values = null)
+    {
+        $this->pdo_statement = $this->pdo_connection->prepare($query);
+
+        if(!is_null($values) && is_array($values) && is_countable($values))
+        {
+            $this->pdo_stmt_values = $values;
+        }
+    }
+
+    /**
+     * executes pdo statement and if it's gonna suppose to return value it will return value
+     * @param bool $has_return_value default is false
+     * @return mixed
+     */
+    public function execute(bool $has_return_value = false)
+    {
+        $this->pdo_statement->execute((is_countable($this->pdo_stmt_values)) ? $this->pdo_stmt_values : null);
+        if($has_return_value){
+            return $this->pdo_statement->fetchAll();
+        }
+    }
+
+    /**
+     * Destroys pdo connection
+     */
+    public function __destruct()
+    {
+        if (!empty($this->pdo_connection) && !is_null($this->pdo_connection))
+        {
+            unset($this->pdo_connection);
+        }
+    }
+
+
 }
