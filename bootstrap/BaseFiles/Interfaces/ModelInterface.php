c64789a5c145d1177d007173db054c5464e61a34
mr_apr_ <mamali0938@gmail.com>
2020-07-26 15:26:38 +0430

    Create model class and model interface for creating models in framework

---
+++ bootstrap/BaseFiles/Interfaces/ModelInterface.php
@@ -0,0 +1,65 @@
+<?php
+
+
+/**
+ * Interface that every model should obey (implement)
+ * Interface ModelInterface
+ */
+interface ModelInterface
+{
+    /**
+     * Prepares pdo helper
+     * @return mixed
+     */
+    public function __construct();
+
+    /**
+     * Closes PDO Connection made by helper
+     * @return mixed
+     */
+    public function closePDO();
+
+    /**
+     * Selecting data from defined table
+     * @param string $statement
+     * @param array $params
+     * @return mixed
+     */
+    public function select(string $statement = null, array $params = []);
+
+    /**
+     * Inserting data in defined table
+     * @param array $params
+     * @return mixed
+     */
+    public function insert(array $params);
+
+    /**
+     * Updating rows in desired table
+     * @param array $params
+     * @param string $condition
+     * @return mixed
+     */
+    public function update(array $params, string $condition = null);
+
+    /**
+     * Deleting data from desired table
+     * @param string $condition
+     * @param array $params
+     * @return mixed
+     */
+    public function delete(string $condition = null, array $params = []);
+
+    /**
+     * Preparing statement
+     * @return mixed
+     */
+    public function prepareStatement();
+
+    /**
+     * Executing prepared statement
+     * @param bool $is_select
+     * @return mixed
+     */
+    public function execute(bool $is_select = false);
+}
